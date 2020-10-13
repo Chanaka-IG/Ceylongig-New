d6130fc2b633a8e43a198242b2e58204111b04ca
chanaka prasad <igchanaka.prasad@gmail.com>
2020-10-13 17:43:26 +0530

    header footer added

---
+++ include/footer1.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+  
+    <link rel="stylesheet" href="./css/footer1.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    
+  </head>
+  <body>
+
+<footer>
+      <div class="main-content">
+        <div class="left box">
+          <h2>
+About us</h2>
+<div class="content1">
+            <p>
+            CeylonGig is established with a motive of creating a web-based platform for all self-employed workers, also known as freelancers,
+                to deliver their service efficiently to the customers who are seeking them.
+</p>
+<div class="social">
+              <a href="#"><span class="fab fa-facebook-f"></span></a>
+              <a href="#"><span class="fab fa-twitter"></span></a>
+              <a href="#"><span class="fab fa-instagram"></span></a>
+              <a href="#"><span class="fab fa-youtube"></span></a>
+            </div>
+</div>
+</div>
+<div class="center box">
+          <h2>
+Address</h2>
+<div class="content1">
+            <div class="place">
+              <span class="fas fa-map-marker-alt"></span>
+              <span class="text">--------------</span>
+            </div>
+<div class="phone">
+              <span class="fas fa-phone-alt"></span>
+              <span class="text">+094-0000000</span>
+            </div>
+<div class="email">
+              <span class="fas fa-envelope"></span>
+              <span class="text">abc@example.com</span>
+            </div>
+</div>
+</div>
+<div class="right box">
+          <h2>
+Contact us</h2>
+<div class="content1">
+            <form action="#">
+              <div class="email">
+                <div class="text">
+Email *</div>
+<input type="email" required>
+              </div>
+<div class="msg">
+                <div class="text">
+Message *</div>
+<!-- NOTE: Due to more textarea tag I got an error. So I changed the textarea name to changeit. Please change that changeit name to textarea -->
+<textarea class="msgForm" rows="2" cols="25" required></textarea> <!-- replace this changeit name to textarea -->
+              
+              <br />
+<div class="btn">
+<button type="submit">Send</button>
+              </div>
+<div class="bottom">
+<center>
+          
+          <span class="far fa-copyright"></span> 2020 All rights reserved.
+        </center>
+</div>
---
+++ include/footer3.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+   
+    <link rel="stylesheet" href="./css/footer3.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+
+<footer>
+      <div class="main-content">
+        <div class="left box">
+          <h2>
+About us</h2>
+<div class="content1">
+            <p>
+            CeylonGig is established with a motive of creating a web-based platform for all self-employed workers, also known as freelancers,
+                to deliver their service efficiently to the customers who are seeking them.</p>
+<div class="social">
+              <a href="#"><span class="fab fa-facebook-f"></span></a>
+              <a href="#"><span class="fab fa-twitter"></span></a>
+              <a href="#"><span class="fab fa-instagram"></span></a>
+              <a href="#"><span class="fab fa-youtube"></span></a>
+            </div>
+</div>
+</div>
+<div class="center box">
+          <h2>
+Address</h2>
+<div class="content1">
+            <div class="place">
+              <span class="fas fa-map-marker-alt"></span>
+              <span class="text">-----------------</span>
+            </div>
+<div class="phone">
+              <span class="fas fa-phone-alt"></span>
+              <span class="text">+094-0000000</span>
+            </div>
+<div class="email">
+              <span class="fas fa-envelope"></span>
+              <span class="text">abc@example.com</span>
+            </div>
+</div>
+</div>
+<div class="right box">
+          <h2>
+Contact us</h2>
+<div class="content1">
+            <form action="#">
+              <div class="email1">
+                <div class="text">
+Email *</div>
+<input type="email" required>
+              </div>
+<div class="msg">
+                <div class="text">
+Message *</div>
+
+<textarea id=".msgForm" rows="2" cols="25" required></textarea> 
+              
+              <br />
+<div class="btn1">
+<button type="submit">Send</button1>
+              </div>
+<div class="bottom">
+<center>
+         
+          <span class="far fa-copyright"></span> 2020 All rights reserved.
+        </center>
+</div>
+
+</footer>
+
+</html>
